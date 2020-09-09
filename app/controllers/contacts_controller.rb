eb3ea2cb63ebfc7ff2c61bf5bac84a6d572f2764
Daniela Sinclair <dasfluid@gmail.com>
2020-09-09 09:59:05 +0100

    Added form

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,21 @@
+class ContactsController < ApplicationController
+  
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(params[:contact])
+    
+    if @contact.save
+      ContactsMailer.general_message(@contact).deliver
+      render :thanks
+    else
+      render :new
+    end
+  end
+
+  def thanks
+  end
+    
+end
